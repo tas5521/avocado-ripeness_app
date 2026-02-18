b64a1ebb4e2e79ac452e7c0e4d5a7a605309e758
Tas5521 <tsbs11152@gmail.com>
2026-02-18 22:29:22 +0900

    クラス名の表示を変更

--- avocado_ripeness_app/lib/constants/class_names.dart
+++ avocado_ripeness_app/lib/constants/class_names.dart
@@ -1,8 +1,8 @@
 /// アボカド成熟度のクラス名定義
 const Map<int, String> CLASS_NAMES = {
-  0: "未熟 (Unripe)",
-  1: "やや未熟 (Slightly Unripe)",
-  2: "適熟 (Ripe)",
-  3: "やや過熟 (Slightly Overripe)",
-  4: "過熟 (Overripe)"
+  0: "未熟\n (Unripe)",
+  1: "やや未熟\n (Slightly Unripe)",
+  2: "適熟\n (Ripe)",
+  3: "やや過熟\n (Slightly Overripe)",
+  4: "過熟\n (Overripe)"
 };
